b41ccbb8cfac914054f3eeff72b40fcda1a8ddc9
xAvi2050 <cheetahmindset7@gmail.com>
2026-04-09 11:39:08 +0530

    Enable Turbopack caching

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+    turbopackFileSystemCacheForBuild: true,
+  },
 };
 
 export default nextConfig;
